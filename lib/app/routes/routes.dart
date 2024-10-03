d374c38d4a7b5374c210d61375a790751117bed3
ra7mouch <rahmabenseghaier@gmail.com>
2024-10-03 16:03:43 +0200

    rahmacommit

---
+++ lib/app/routes/routes.dart
@@ -0,0 +1,17 @@
+import 'package:aswan/app/app.dart';
+import 'package:aswan/presentation/home/view/home_page.dart';
+import 'package:aswan/presentation/login/view/login_page.dart';
+import 'package:flutter/widgets.dart';
+
+
+List<Page<dynamic>> onGenerateAppViewPages(
+  AppStatus state,
+  List<Page<dynamic>> pages,
+) {
+  switch (state) {
+    case AppStatus.authenticated:
+      return [HomePage.page()];
+    case AppStatus.unauthenticated:
+      return [LoginPage.page()];
+  }
+}
